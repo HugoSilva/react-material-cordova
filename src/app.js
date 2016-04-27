cfea6702187d5f062ae72261545c26fb64f9104f
Hugo Silva <Hugo Silva>
2016-04-27 01:31:26 +0100

    Update to all the dependencies
    I wanted to have all the most recent version of every dependency in use,
    the only dependency without a stable release is material-ui, this will
    be re viewed in the future.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import { render } from 'react-dom'
 import routes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 require('./index.html');
 
-let injectTapEventPlugin = require('react-tap-event-plugin');
 injectTapEventPlugin();
 
 render(routes, document.getElementById('app'))
